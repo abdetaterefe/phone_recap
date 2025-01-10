26165ad99054e0477d726db07bf9e339d0293501
Abdi Terefe <abdetaterefe@gmail.com>
2025-01-10 16:19:24 +0300

    test fixed

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:phone_recap/app/app.dart';
-import 'package:phone_recap/counter/counter.dart';
+import 'package:phone_recap/home/home.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders HomePage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(HomePage), findsOneWidget);
     });
   });
 }
