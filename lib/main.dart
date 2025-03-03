b625368f74094d4a90370c94e57c9fb814289688
Abdi Terefe <abdetaterefe@gmail.com>
2025-03-03 08:50:27 -0800

    firebase crashlytics and analytics added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:phone_recap/app/app.dart';
 import 'package:phone_recap/app/services/services.dart';
 import 'package:phone_recap/app/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:phone_recap/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack);
+    return true;
+  };
   AppTheme initialTheme = await ThemeService.getTheme();
   runApp(
     MultiBlocProvider(
